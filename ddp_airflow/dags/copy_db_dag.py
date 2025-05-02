90e2fe8fa3c8cc8dd4deb10fc6ac391344e81991
mubaraq <mubskill@gmail.com>
2025-05-02 03:04:52 -0500

    Added transforamation task

--- ddp_airflow/dags/copy_db_dag.py
+++ ddp_airflow/dags/copy_db_dag.py
@@ -1,12 +1,14 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.providers.standard.operators.python import PythonOperator
 
 from src.copy_file import perform_copy_data
+from src.clean import sanitize_and_obfuscate
+
 
 
 from datetime import datetime, timedelta
 
-import psycopg2
+
 import json
 
 
@@ -29,15 +31,17 @@ default_args = {
 
 
 with DAG(
-    dag_id = 'copy_db',
+    dag_id = 'bank_elt',
     default_args = default_args,
-    description = "This copies task from prod db to destinations",
+    description = "Performs daily ELT: copies bank data from prod to clean DB, sanitizes it, and loads it into a dev database.",
     start_date = datetime(2025, 4, 29, 2),
-    schedule_interval = None
+    schedule_interval = '@daily'
 
 ) as dag:
-    copy_to_sanitized = PythonOperator(
-        task_id = 'copy_to_sanitized',
+    #extract from production to cleandb
+    copy_to_clean = PythonOperator(
+        task_id = 'copy_to_clean',
+        description = "Loads raw data from production to clean database",
         python_callable = perform_copy_data,
         op_kwargs=
         {
@@ -49,33 +53,35 @@ with DAG(
         },
         retry_delay = timedelta(minutes = dest[0]["retry_delay"]) 
     )
+    
+    # sanitize and obfuscate data
+    transform_data = PythonOperator(
+        task_id = 'sanitize_and_obfuscate',
+        description = "Sanitizes and obfuscates sensitive fields in the clean bank data (e.g., name, age, contact).",
+        python_callable= sanitize_and_obfuscate,
+        retry_delay = timedelta(minutes = dest[0]["retry_delay"])
+    )
+    
 
-    copy_to_obfuscated = PythonOperator(
-        task_id = 'copy_to_obfuscated',
+    #copy to mini db
+    copy_to_dev = PythonOperator(
+        task_id = 'copy_to_dev',
+        description = "Loads clean and minimized data to db",
         python_callable = perform_copy_data,
         op_kwargs=
         {
             "dest_table": dest[1]["destination_table"],
+            "source_db" : dest[0]["name"], #calls the copy_file(i.e transformed)
             "dest_db_name": dest[1]["name"],
             "source" : dest[1]["source"],
-            "batch_size" : dest[1]["batch_size"]
-             
+            "batch_size" : dest[1]["batch_size"],
+            "filter_criteria" : dest[1]["filter_criteria"]
         },
         retry_delay = timedelta(minutes = dest[1]["retry_delay"]) 
     )
 
-    copy_to_dev = PythonOperator(
-        task_id = 'copy_to_dev',
-        python_callable = perform_copy_data,
-        op_kwargs=
-        {
-            "dest_table": dest[2]["destination_table"],
-            "dest_db_name": dest[2]["name"],
-            "source" : dest[2]["source"],
-            "batch_size" : dest[2]["batch_size"],
-            "filter_criteria" : dest[2]["filter_criteria"]
-        },
-        retry_delay = timedelta(minutes = dest[2]["retry_delay"]) 
-    )
-    #sset task dependencies
-    copy_to_sanitized >> copy_to_obfuscated >> copy_to_dev
+
+
+    #set task dependencies (ELT)
+
+    copy_to_clean >> transform_data >> copy_to_dev
