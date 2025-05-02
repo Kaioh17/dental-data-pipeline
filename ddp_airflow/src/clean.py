93ef2a285496538753750bf7b4c145e58c88466d
mubaraq <mubskill@gmail.com>
2025-05-02 03:05:59 -0500

    Added sanitization functions

--- ddp_airflow/src/clean.py
+++ ddp_airflow/src/clean.py
@@ -1,17 +1,54 @@
 import psycopg2
 import copy_file
 
-copy_file.connect_to_prod_db()
 
-## we have an operator that will connect to the production db 
-## connecttodb >> sanitize >> obfuscate
 
-def obfuscation():
-    ### connect to db
-    # - write code for obfuscation 
-    copy_file.connect_to_temp_db("obfuscated_bank_db")
+def anonymize_data(data):
+    
+    ##AGE JOB MARITAL EDUCATION DEFAULT BALANCE HOUSing LOAN CONTACT 
+    data['Age'] = None
+    data['Job'] = 'unknown'
+    data['marital'] = 'unknown'
+    data['education']  =  'unknown' 
+    data['balanced']  =  0
+    data['loan']  =  'unknown' 
+    data['contact']  =  'redacted'  
+    
+    return data
+def redact_data(data):
     pass
 
-def sanitazation():
-    copy_file.connect_to_temp_db("sanitize_bank_db")
+def mask_data(data):
     pass
+
+def tokenize_data(data):
+    pass
+
+def aggregate_age(data):
+    pass
+
+
+
+#python operation sanotzong an dobfuscattion
+def sanitize_and_obfuscate():
+    conn= copy_file.connect_to_temp_db("clean_bank_db")
+    cur = conn.cursor()
+
+    #anonymize data
+    column = {}
+    data = anonymize_data(column)
+
+    set_clause  = ",".join([f"{k} = {v}" for k,v in data.items()])
+    cur.execute(f"UPDATE bank_table SET {set_clause};")
+    # print(f"UPDATE bank_table SET {set_clause};")
+
+
+    conn.commit()
+    copy_file.close_db(conn,cur)
+
+
+
+sanitize_and_obfuscate()
+
+
+    
